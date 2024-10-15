310df140759869b464e8e82cb09de880f416b47d
lds-engineers <contact@ldsengineers.com>
2024-10-15 21:20:20 +0530

    void duty arrangent issue fixed
    void duty arrangent issue fixed

--- pushback/DriverAndCabDetails.php
+++ pushback/DriverAndCabDetails.php
@@ -17,6 +17,22 @@ if(orixPushback::getAssignmentDetails($bookingId)) {
 }
 
 $result = [];
+if($reassignment){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, CURL_URL.'void_duty');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($callVoidDuty['data']));
+    $headers = array();
+    $headers[] = 'Content-Type: application/json';
+    $headers[] = 'rqid: '.orixPushback::Rqid($callVoidDuty['data']);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    $result = curl_exec($ch);
+    $inputString = trim($result);
+    $inputString = $CFG->real_escape_string($inputString);
+    orixPushback::InsertPushbackLog($bookingId, json_encode($callVoidDuty['data']), $inputString, 'void_duty', json_encode($data));
+}
+    
 if($DriverAndCabDetails['status']) {
     /* 1. Driver Assignment*/
     /*Call curl request start*/
@@ -33,21 +49,6 @@ if($DriverAndCabDetails['status']) {
     // print_r($DriverAndCabDetails['data']);
     $inputString = trim($result);
     $inputString = $CFG->real_escape_string($inputString);
-    if($reassignment){
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, CURL_URL.'void_duty');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($callVoidDuty['data']));
-        $headers = array();
-        $headers[] = 'Content-Type: application/json';
-        $headers[] = 'rqid: '.orixPushback::Rqid($callVoidDuty['data']);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        $result = curl_exec($ch);
-        $inputString = trim($result);
-        $inputString = $CFG->real_escape_string($inputString);
-        orixPushback::InsertPushbackLog($bookingId, json_encode($callVoidDuty['data']), $inputString, 'void_duty', json_encode($data));
-    }
     orixPushback::InsertPushbackLog($bookingId, json_encode($DriverAndCabDetails['data']), $inputString, 'assigned', json_encode($data));
     $result = json_decode($result);
     if (curl_errno($ch)) {
